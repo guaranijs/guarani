df2f7267bcc6d030895bacb91f019873cd2c9b75
Eduardo Rezende <eduardorbr7@gmail.com>
2021-03-08 01:59:10 -0300

    Added JWS Algorithms.

--- packages/jose/lib/jwa/jws/none.ts
+++ packages/jose/lib/jwa/jws/none.ts
@@ -2,7 +2,7 @@ import type { JsonWebKey } from '../../jwk'
 import { JWSAlgorithm } from './algorithm'
 
 class None extends JWSAlgorithm {
-  public constructor () { super(undefined) }
+  public constructor () { super(undefined, 'none') }
 
   public sign (data: Buffer, key?: JsonWebKey): string { return '' }
   public verify (signature: string, data: Buffer, key?: JsonWebKey): void {}

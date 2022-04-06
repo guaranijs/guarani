ab7289a4e343dc847ca877fdc15c5fc2e6da6142
Eduardo Ribeiro Rezende <eduardorbr7@gmail.com>
2022-04-06 11:03:07 -0300

    chore(oauth2-server): initial setup

---
+++ packages/oauth2-server/jest.config.ts
@@ -0,0 +1,9 @@
+import { join } from 'path';
+
+import baseConfig from '../../jest.config';
+
+export default {
+  ...baseConfig,
+  rootDir: '../../',
+  testMatch: [join(__dirname, 'tests/**/*.spec.ts')],
+};

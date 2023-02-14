da2f7c7d68734bf23a9ef18c10736b916b0fe372
Eduardo Rezende <eduardorbr7@gmail.com>
2023-02-14 03:55:49 +0000

    feat(oauth2-server): add id token claims and claims parameters

---
+++ packages/oauth2-server/src/lib/id-token/id-token.claims.parameters.ts
@@ -0,0 +1,71 @@
+import { JsonWebTokenClaimsParameters } from '@guarani/jose';
+
+/**
+ * OpenID Connect ID Token Claims Parameters.
+ *
+ * @see https://openid.net/specs/openid-connect-core-1_0.html#IDToken
+ */
+export interface IdTokenClaimsParameters extends JsonWebTokenClaimsParameters {
+  /**
+   * Identifier of the Issuer of the ID Token.
+   */
+  readonly iss: string;
+
+  /**
+   * Subject represented by theID  Token.
+   */
+  readonly sub: string;
+
+  /**
+   * Identifier of the Audience the ID Token is intended to.
+   */
+  readonly aud: string | string[];
+
+  /**
+   * UTC time denoting the Expiration Time of the ID Token.
+   */
+  readonly exp: number;
+
+  /**
+   * UTC time denoting the moment when the ID Token was created.
+   */
+  readonly iat: number;
+
+  /**
+   * Time when the End-User was authenticated.
+   */
+  readonly auth_time?: number;
+
+  /**
+   * Value provided at the Authentication Request to be passed unmodified to the ID Token.
+   */
+  readonly nonce?: string;
+
+  /**
+   * Authentication Context Class Reference value that identifies the Authentication Context Class
+   * that the authentication performed satisfied.
+   */
+  readonly acr?: string;
+
+  /**
+   * Authentication Methods References.
+   */
+  readonly amr?: string[];
+
+  /**
+   * Authorized party to which the ID Token was issued.
+   */
+  readonly azp?: string;
+
+  /**
+   * Base64Url encoding of the left-most half of the hash of the octets of the ASCII **access_token**,
+   * where the hash algorithm is the one used in the **alg** JSON Web Signature Header Parameter of the ID Token.
+   */
+  readonly at_hash?: string;
+
+  /**
+   * Base64Url encoding of the left-most half of the hash of the octets of the ASCII **code**,
+   * where the hash algorithm is the one used in the **alg** JSON Web Signature Header Parameter of the ID Token.
+   */
+  readonly c_hash?: string;
+}

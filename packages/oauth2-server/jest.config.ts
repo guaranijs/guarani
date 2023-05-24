e0b460cef30a57c98f47811a7b5445963b7ec3d9
revensky <38702533+revensky@users.noreply.github.com>
2023-05-24 19:38:46 -0300

    build: upgrade nx to version 16 (#66)

--- packages/oauth2-server/jest.config.ts
+++ packages/oauth2-server/jest.config.ts
@@ -3,14 +3,15 @@ import path from 'path';
 export default {
   displayName: '@guarani/oauth2-server',
   preset: '../../jest.preset.js',
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.spec.json',
-    },
-  },
+  globals: {},
   testEnvironment: 'node',
   transform: {
-    '^.+\\.[tj]sx?$': 'ts-jest',
+    '^.+\\.[tj]sx?$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+      },
+    ],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   setupFilesAfterEnv: [path.join(__dirname, 'jest.setup.ts')],

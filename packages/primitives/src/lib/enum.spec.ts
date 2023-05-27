9116ecde103b5f3a555539fc980eb57954461a57
Eduardo Rezende <eduardorbr7@gmail.com>
2023-05-27 06:56:36 +0000

    refactor(primitives): enhance enum code

--- packages/primitives/src/lib/enum.spec.ts
+++ packages/primitives/src/lib/enum.spec.ts
@@ -16,22 +16,26 @@ enum StringEnum {
   AnotherFoo = 'foo',
 }
 
-describe('Enum utilities', () => {
+describe('getKeys()', () => {
   it("should return all the members' keys of the enum.", () => {
-    expect(Enum.keys(IntEnum)).toEqual(['One', 'Two', 'Four', 'Eight', 'AnotherOne']);
-    expect(Enum.keys(StringEnum)).toEqual(['Foo', 'Bar', 'Baz', 'Qux', 'AnotherFoo']);
+    expect(Enum.getKeys(IntEnum)).toEqual(['One', 'Two', 'Four', 'Eight', 'AnotherOne']);
+    expect(Enum.getKeys(StringEnum)).toEqual(['Foo', 'Bar', 'Baz', 'Qux', 'AnotherFoo']);
   });
+});
 
-  it('should return "undefined" when the provided key is not a member of the enum.', () => {
-    expect(Enum.key(IntEnum, 3)).toBeUndefined();
-    expect(Enum.key(StringEnum, 'unknown')).toBeUndefined();
+describe('getKey()', () => {
+  it('should return null when the provided key is not a member of the enum.', () => {
+    expect(Enum.getKey(IntEnum, 3)).toBeNull();
+    expect(Enum.getKey(StringEnum, 'unknown')).toBeNull();
   });
 
   it('should return the first member that matches the requested key.', () => {
-    expect(Enum.key(IntEnum, 1)).toBe('One');
-    expect(Enum.key(StringEnum, 'foo')).toBe('Foo');
+    expect(Enum.getKey(IntEnum, 1)).toBe('One');
+    expect(Enum.getKey(StringEnum, 'foo')).toBe('Foo');
   });
+});
 
+describe('hasKey()', () => {
   it('should return whether or not the provided key is a member of the enum.', () => {
     expect(Enum.hasKey(IntEnum, 'Eight')).toBe(true);
     expect(Enum.hasKey(StringEnum, 'Baz')).toBe(true);
@@ -39,22 +43,28 @@ describe('Enum utilities', () => {
     expect(Enum.hasKey(IntEnum, 'Unknown')).toBe(false);
     expect(Enum.hasKey(StringEnum, 'Unknown')).toBe(false);
   });
+});
 
+describe('getValues()', () => {
   it("should return all the members's values of the enum.", () => {
-    expect(Enum.values(IntEnum)).toEqual([1, 2, 4, 8, 1]);
-    expect(Enum.values(StringEnum)).toEqual(['foo', 'bar', 'baz', 'qux', 'foo']);
+    expect(Enum.getValues(IntEnum)).toEqual([1, 2, 4, 8, 1]);
+    expect(Enum.getValues(StringEnum)).toEqual(['foo', 'bar', 'baz', 'qux', 'foo']);
   });
+});
 
-  it('should return "undefined" when the provided value is not a member of the enum.', () => {
-    expect(Enum.parse(IntEnum, 32)).toBeUndefined();
-    expect(Enum.parse(StringEnum, 'unknown')).toBeUndefined();
+describe('parse()', () => {
+  it('should return null when the provided value is not a member of the enum.', () => {
+    expect(Enum.parse(IntEnum, 32)).toBeNull();
+    expect(Enum.parse(StringEnum, 'unknown')).toBeNull();
   });
 
   it('should return the parsed member of the enum.', () => {
     expect(Enum.parse(IntEnum, 2)).toBe(2);
     expect(Enum.parse(StringEnum, 'qux')).toBe('qux');
   });
+});
 
+describe('hasValue()', () => {
   it('should return whether or not the provided value is a member of the enum.', () => {
     expect(Enum.hasValue(IntEnum, 2)).toBe(true);
     expect(Enum.hasValue(StringEnum, 'qux')).toBe(true);
@@ -62,9 +72,11 @@ describe('Enum utilities', () => {
     expect(Enum.hasValue(IntEnum, 32)).toBe(false);
     expect(Enum.hasValue(StringEnum, 'unknown')).toBe(false);
   });
+});
 
+describe('getEntries()', () => {
   it('should return the entries of the enum.', () => {
-    expect(Enum.entries(IntEnum)).toEqual([
+    expect(Enum.getEntries(IntEnum)).toEqual([
       ['One', 1],
       ['Two', 2],
       ['Four', 4],
@@ -72,7 +84,7 @@ describe('Enum utilities', () => {
       ['AnotherOne', 1],
     ]);
 
-    expect(Enum.entries(StringEnum)).toEqual([
+    expect(Enum.getEntries(StringEnum)).toEqual([
       ['Foo', 'foo'],
       ['Bar', 'bar'],
       ['Baz', 'baz'],

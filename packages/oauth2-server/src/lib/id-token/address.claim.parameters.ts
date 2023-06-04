c30ed43f37b998ed8dfefdaf8bab0a5a5938bd57
Eduardo Rezende <eduardorbr7@gmail.com>
2023-06-04 07:09:44 +0000

    refactor(oauth2-server): enhance id token claims

--- packages/oauth2-server/src/lib/id-token/address.claim.parameters.ts
+++ packages/oauth2-server/src/lib/id-token/address.claim.parameters.ts
@@ -1,7 +1,9 @@
+import { Dictionary } from '@guarani/types';
+
 /**
  * Parameters of the OpenID Connect's Userinfo Address Claim.
  */
-export interface AddressClaimParameters {
+export interface AddressClaimParameters extends Dictionary<unknown> {
   /**
    * Formatted Address of the User.
    */

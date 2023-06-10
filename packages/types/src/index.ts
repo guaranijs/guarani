1c70da16756986f43d0bc37dacbecb834c4c2091
Eduardo Rezende <eduardorbr7@gmail.com>
2023-06-10 02:52:08 +0000

    chore(types): fix import and export order

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -1,7 +1,7 @@
 export { AbstractConstructor } from './lib/abstract-constructor.interface';
 export { Comparable } from './lib/comparable.interface';
-export { ConstructorOrInstance } from './lib/constructor-or-instance.type';
 export { Constructor } from './lib/constructor.interface';
+export { ConstructorOrInstance } from './lib/constructor-or-instance.type';
 export { Dictionary } from './lib/dictionary.type';
 export { Nullable } from './lib/nullable.type';
 export { OneOrMany } from './lib/one-or-many.type';

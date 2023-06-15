19658bc07f7c3ca5889e36d3158fb23e23144090
Eduardo Rezende <eduardorbr7@gmail.com>
2023-06-15 04:37:04 +0000

    feat(types): add json types

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -3,5 +3,6 @@ export { Comparable } from './lib/comparable.interface';
 export { Constructor } from './lib/constructor.interface';
 export { ConstructorOrInstance } from './lib/constructor-or-instance.type';
 export { Dictionary } from './lib/dictionary.type';
+export { Json } from './lib/json.type';
 export { Nullable } from './lib/nullable.type';
 export { OneOrMany } from './lib/one-or-many.type';

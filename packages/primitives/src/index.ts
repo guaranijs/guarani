15ba4856186fe7487e9b3d8374a1bc309a18da32
Eduardo Rezende <eduardorbr7@gmail.com>
2023-06-15 04:36:26 +0000

    feat(primitives): add json with prototype pollution protection

--- packages/primitives/src/index.ts
+++ packages/primitives/src/index.ts
@@ -1,4 +1,5 @@
 export { bitstringToBuffer, bufferToBitstring, bufferToInteger } from './lib/buffer';
 export * as Enum from './lib/enum';
 export { integerToBuffer } from './lib/integer';
+export * as JSON from './lib/json';
 export { isPlainObject, removeNullishValues } from './lib/objects';

4ca5d2091edf1796e7ce465cdb40d1902b97ada3
Eduardo Rezende <eduardorbr7@gmail.com>
2023-11-02 23:09:58 +0000

    fix(primitives): fix tests and code documentation

--- packages/primitives/src/lib/json.ts
+++ packages/primitives/src/lib/json.ts
@@ -15,6 +15,7 @@ function stripForbiddenProperties(key: string, value: any, reviver?: (this: any,
  * @param text A valid JSON string.
  * @param reviver A function that transforms the results. This function is called for each member of the object.
  * If a member contains nested objects, the nested objects are transformed before the parent object is.
+ * @returns Parsed object from the JSON string.
  */
 export function parse(text: string, reviver?: (this: any, key: string, value: any) => any): any {
   return JSON.parse(text, (key, value) => stripForbiddenProperties(key, value, reviver));
@@ -26,6 +27,7 @@ export function parse(text: string, reviver?: (this: any, key: string, value: an
  * @param value A JavaScript value, usually an object or array, to be converted.
  * @param replacer A function that transforms the results.
  * @param space Adds indentation, white space, and line break characters to the return-value JSON text to make it easier to read.
+ * @returns Stringified object.
  */
 export function stringify(
   value: any,
@@ -39,6 +41,7 @@ export function stringify(
  * @param value A JavaScript value, usually an object or array, to be converted.
  * @param replacer An array of strings and numbers that acts as an approved list for selecting the object properties that will be stringified.
  * @param space Adds indentation, white space, and line break characters to the return-value JSON text to make it easier to read.
+ * @returns Stringified object.
  */
 export function stringify(value: any, replacer?: (number | string)[] | null, space?: string | number): string;
 
@@ -46,9 +49,10 @@ export function stringify(value: any, replacer?: (number | string)[] | null, spa
  * Converts a JavaScript value to a JavaScript Object Notation (JSON) string.
  *
  * @param value A JavaScript value, usually an object or array, to be converted.
- * @param replacer A function that transforms the results or
- * an array of strings and numbers that acts as an approved list for selecting the object properties that will be stringified.
+ * @param replacer A function that transforms the results or an array of strings and numbers that acts
+ * as an approved list for selecting the object properties that will be stringified.
  * @param space Adds indentation, white space, and line break characters to the return-value JSON text to make it easier to read.
+ * @returns Stringified object.
  */
 export function stringify(value: any, replacer?: any, space?: string | number): string {
   return JSON.stringify(value, replacer, space);
